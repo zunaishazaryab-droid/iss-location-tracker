11f02fde227f34974f5ce88a40b086e93354cc98
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-05 16:17:20 +0500

    Fix URL typo and improve error handling for ISS location tracking

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-nows.json")
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
-if response.status_code == 404:
-    raise Exception("The thing you are looking for not exists")
-else:
-    print("Here you Go")
+response.raise_for_status()
+
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
 
