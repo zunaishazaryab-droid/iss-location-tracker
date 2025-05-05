39bbb3b5a3199c8ffe4b933eb46fb66057725417
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-05 17:11:12 +0500

    Refactor ISS location tracking code and add sunrise/sunset data retrieval

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
-response.raise_for_status()
+MY_LAT = 30.969349
+MY_LONG = 70.942795
 
-data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
-print(iss_position)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response)
+# response.raise_for_status()
+
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+# print(iss_position)
+parameters = {
+    "lat":MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+sunset_response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+sunset_response.raise_for_status()
+data = sunset_response.json()
+# print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
+
+date_now = datetime.now()
+print(date_now.hour)
 
